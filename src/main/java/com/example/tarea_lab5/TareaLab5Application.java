23445d64a5a128048e885b4c2f25dda7e8b4f0b8
oshixmi <oshin.ccaico@pucp.edu.pe>
2026-05-09 18:30:45 -0500

    primer commit

---
+++ src/main/java/com/example/tarea_lab5/TareaLab5Application.java
@@ -0,0 +1,13 @@
+package com.example.tarea_lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaLab5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaLab5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tarea_lab5/TareaLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea_lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaLab5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
